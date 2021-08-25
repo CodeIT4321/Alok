5596319d3a0f84af41d41997d6ce6ecdedb43563
Mausam Thapa <mausamthapa@Mausams-MacBook-Pro.local>
2021-08-25 22:00:45 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Alok
         </a>
       </header>
     </div>
